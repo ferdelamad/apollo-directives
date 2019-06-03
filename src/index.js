dae15596bc8e600fd83835944bfc048f6f793f26
ferdelamad <fernando.delamadrid@checkr.com>
2019-06-03 07:37:10 -0700

    Add string format directives s 🖋

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { SchemaDirectiveVisitor } from 'graphql-tools';
 import { defaultFieldResolver } from 'graphql';
+import lodash from 'lodash';
 
-export class RenameFieldDirective extends SchemaDirectiveVisitor {
+export class renameField extends SchemaDirectiveVisitor {
   visitFieldDefinition(field) {
     const { name } = this.args;
     const { resolve = defaultFieldResolver } = field;
@@ -12,3 +13,27 @@ export class RenameFieldDirective extends SchemaDirectiveVisitor {
     };
   }
 }
+
+const createFormatStringDirective = method => (
+  class extends SchemaDirectiveVisitor {
+    visitFieldDefinition(field) {
+      const transform = input =>
+        typeof input === 'string' ? lodash[method](input) : input
+      const { resolve = defaultFieldResolver } = field;
+      field.resolve = async function(...args) {
+        const result = await resolve.apply(this, args);
+        return Array.isArray(result) ? result.map(transform) : transform(result)
+      };
+    }
+  }
+);
+
+export const camelCase = createFormatStringDirective(`camelCase`);
+export const capitalize = createFormatStringDirective(`capitalize`);
+export const lowerCase = createFormatStringDirective(`lowerCase`);
+export const lowerFirst = createFormatStringDirective(`lowerFirst`);
+export const toLower = createFormatStringDirective(`toLower`);
+export const toUpper = createFormatStringDirective(`toUpper`);
+export const trim = createFormatStringDirective(`trim`);
+export const upperCase = createFormatStringDirective(`upperCase`);
+export const upperFirst = createFormatStringDirective(`upperFirst`);
